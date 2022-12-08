7a64ff7fc01d9ff43d5aefe284cb960befbb92e8
Aleksandr <aleksanches123@mail.ru>
2022-12-08 10:05:27 +0300

    Задача 6.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1() {
@@ -35,6 +36,12 @@ public class Main {
         System.out.println("Задача 5");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + "год является високосным");
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.print(i + " ");
         }
     }
 }
