c4df62fae1d31bb3133b28e6712ef41ad11518bc
Aleksandr <aleksanches123@mail.ru>
2022-12-08 10:49:20 +0300

    Задача 10.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         task7();
         task8();
         task9();
+        task10();
     }
 
     public static void task1() {
@@ -79,6 +80,15 @@ public class Main {
             percent = sum / 100 + deposit / 100;
             sum = sum + deposit + percent;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + sum + " рублей");
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        int sum;
+        for (int i = 1; i <= 10; i++) {
+            sum = 2 * i;
+            System.out.println("2*" + i + "=" + sum);
         }
     }
 }
